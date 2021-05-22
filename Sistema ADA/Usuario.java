1d6c5ab1e99b13256321f3e6ec081ad714159269
CarolShine <carolinecg431@gmail.com>
2021-05-22 08:52:48 -0300

    Criei página de usuário

---
+++ Sistema ADA/Usuario.java
@@ -0,0 +1,58 @@
+package br.ufpb.adocaoanimais;
+import java.util.List;
+
+public class Usuario {
+
+	private String nome;
+
+	private String cpf;
+
+	private String dataNascimento;
+
+	private List<Requisito> requisitos;
+
+	public Usuario(String nome, String cpf, String dataNascimento, List<Requisito> requisitos) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.requisitos = requisitos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Requisito> getRequisitos() {
+		return requisitos;
+	}
+
+	public void adicionaRequisito(Requisito req) {
+		this.requisitos.add(req);
+	}
+	
+	public void setRequisitos(List<Requisito> requisitos) {
+		this.requisitos = requisitos;
+	}
+
+}
